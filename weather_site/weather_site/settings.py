4618e9d8675467fefcf0884978ce19c05279edbb
Prakriti Timilsina <98209027+Dynamic-prakriti@users.noreply.github.com>
2023-07-30 19:14:51 +0545

    Update settings.py

--- weather_site/weather_site/settings.py
+++ weather_site/weather_site/settings.py
@@ -19,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k_(a=leddy#)rjc7rnexeyf-nniout^m^+ee_x!!q-dobw^9bs'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
